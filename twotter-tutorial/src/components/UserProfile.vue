3d4ccb68ede08aea5f08c726fff3655a3bb3ad11
saggers <sagarparmar0233@gmail.com>
2020-12-12 19:05:25 -0800

    practising continuous to build the twoot application

--- twotter-tutorial/src/components/UserProfile.vue
+++ twotter-tutorial/src/components/UserProfile.vue
@@ -16,7 +16,29 @@
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea
+          id="newTwoot"
+          name="newTwoot"
+          rows="4"
+          v-model="newTwootContent"
+        />
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option
+              :value="option.value"
+              v-for="(option, index) in twootTypes"
+              :key="index"
+              >{{ option.name }}</option
+            >
+          </select>
+        </div>
+        <button>Twoot!</button>
+      </form>
     </div>
+
     <div class="user-profile__twoots-wrapper">
       <TwootItem
         v-for="twoot in user.twoots"
@@ -37,6 +59,12 @@ export default {
   components: { TwootItem },
   data() {
     return {
+      newTwootContent: "",
+      selectedTwootType: "instant",
+      twootTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twoot" },
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -44,7 +72,7 @@ export default {
         firstname: "Saggers",
         lastname: "Par",
         email: "sagar@gmail.com",
-        is_admin: false,
+        is_admin: true,
         twoots: [
           { id: 1, content: "Twooter is amazing!" },
           { id: 2, content: "Don't forget to subscribe to earth is square!" },
@@ -70,6 +98,15 @@ export default {
     },
     toggleFavourite(id) {
       console.log(`Favourite Tweeet #${id}`);
+    },
+    createNewTwoot() {
+      if (this.newTwootContent != "" && this.selectedTwootType !== "draft") {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent,
+        });
+      }
+      this.newTwootContent = "";
     },
   },
   mounted() {
@@ -108,6 +145,14 @@ h1 {
   border-radius: 5px;
   margin: 5px 0;
   padding: 3px 10px;
+  margin-bottom: 20px;
   margin-right: auto;
+}
+
+.user-profile__create-twoot {
+  padding-top: 20px;
+  border-top: 1px solid #dfe3eb;
+  display: flex;
+  flex-direction: column;
 }
 </style>
